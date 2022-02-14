fcf5a4e6330ad5191903f094cb213712c18748dc
Abhishek Potdar <abhirpotdar@gmail.com>
2022-02-14 23:49:40 +0530

    Latest Pull

--- src/views/Home-Normal-Grid.vue
+++ src/views/Home-Normal-Grid.vue
@@ -1,29 +1,31 @@
 
  <template>
-  <HeaderContainer title="Cllct" />
-  <HeroCard />
+   <ion-page>
+    <ion-content :fullscreen="true">
+        <HeaderContainer title="Cllct" />
+        <HeroCard />
 
-  <div class="portfolio-normal-grid">
-    <div class="container mx-auto">
-        <div class="flex flex-wrap">
-            <div class="w-1/5 xs:w-1/2">
-                <div class="portfolio-box">
-                    <div class="portfolio-image">
-                        <img src="../../resources/home-page/portfolios/Portfolio-1.jpg">
-                    </div>
-                    <div class="portfolio-content">
-                        <div class="portfolio-title flex">
-                            <span class="color-badge"></span>
-                            <h4>from web design Ledger</h4>
+        <div class="portfolio-normal-grid">
+            <div class="container mx-auto">
+                <div class="flex flex-wrap">
+                    <div class="w-1/5 xs:w-1/2">
+                        <div class="portfolio-box">
+                            <div class="portfolio-image">
+                                <img src="../../resources/home-page/portfolios/Portfolio-1.jpg">
+                            </div>
+                            <div class="portfolio-content">
+                                <div class="portfolio-title flex">
+                                    <span class="color-badge"></span>
+                                    <h4>from web design Ledger</h4>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  </div>  
-
-
+    </ion-content>
+   </ion-page>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
