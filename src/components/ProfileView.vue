60010c2a22f65cfe95d58d27b6153ccf601a8976
Abhimanyu Patil <chanondler@Abhimanyus-MacBook-Air.local>
2022-04-07 18:37:19 +0530

    edit profile

--- src/components/ProfileView.vue
+++ src/components/ProfileView.vue
@@ -27,10 +27,10 @@
           <h5 class="font-aileron text-[#7630B4] text-sm mb-1">Email</h5>
           <input
             v-if="mode === 'edit'"
+            disabled
             v-model="editEmail"
             type="email"
-            @input="editEmail = event.target.value"
-            class="bg-[#ECECEC] text-xs text-black border border-grey w-full px-2 py-2 rounded-xl font-poppins"
+            class="bg-[#ECECEC] text-xs text-black border border-grey w-full px-2 py-2 rounded-xl font-poppins disabled:bg-zinc-400 disabled:cursor-not-allowed"
           />
           <p
             v-if="mode === 'view'"
@@ -45,7 +45,7 @@
             v-if="mode === 'edit'"
             v-model="editName"
             type="text"
-            @input="editName = event.target.value"
+            @input="(event) => (editName = event.target.value)"
             class="bg-[#ECECEC] text-xs text-black border border-grey w-full px-2 py-2 rounded-xl font-poppins"
           />
           <p
@@ -61,7 +61,7 @@
             v-if="mode === 'edit'"
             v-model="editUserName"
             type="text"
-            @input="editUserName = event.target.value"
+            @input="(event) => (editUserName = event.target.value)"
             class="bg-[#ECECEC] text-xs text-black border border-grey w-full px-2 py-2 rounded-xl font-poppins"
           />
           <p
@@ -73,62 +73,67 @@
         </div>
         <div class="profile-details-item mb-3">
           <h5 class="font-aileron text-[#7630B4] text-sm mb-1">Gender</h5>
-          <div v-if="mode === 'edit'" class="flex justify-between flex-wrap">
-            <div class="form-check flex items-center">
-              <input
-                @click="editGender === 'male'"
-                class="form-check-input appearance-none rounded-full h-3 w-3 border border-gray-300 bg-white checked:bg-primary checked:border-purple-500 focus:outline-none transition duration-200 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                type="radio"
-                name="male"
-                @change="editGender === 'male'"
-                value="male"
-                id="male"
-                :checked="editGender?.toLowerCase() === 'male'"
-              />
-              <label
-                class="form-check-label inline-block text-gray-800 text-sm font-aileron"
-                for="usergender"
-              >
-                Male
-              </label>
-            </div>
-            <div class="form-check flex items-center">
-              <input
-                v-model="editGender"
-                @change="editGender === 'female'"
-                @click="editGender === 'female'"
-                :checked="editGender?.toLowerCase() === 'female'"
-                class="form-check-input appearance-none rounded-full h-3 w-3 border border-gray-300 bg-white checked:bg-primary checked:border-purple-500 focus:outline-none transition duration-200 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                type="radio"
-                name="female"
-                id="female"
-                value="female"
-              />
-              <label
-                class="form-check-label inline-block text-gray-800 text-sm font-aileron"
-                for="usergender"
-              >
-                Female
-              </label>
-            </div>
-            <div class="form-check flex items-center mt-2">
-              <input
-                @change="editGender === 'na'"
-                @click="editGender === 'na'"
-                :checked="editGender?.toLowerCase() === 'na'"
-                class="form-check-input appearance-none rounded-full h-3 w-3 border border-gray-300 bg-white checked:bg-primary checked:border-purple-500 focus:outline-none transition duration-200 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                type="radio"
-                name="na"
-                id="na"
-                value="na"
-              />
-              <label
-                class="form-check-label inline-block text-gray-800 text-sm font-aileron"
-                for="usergender"
-              >
-                Rather Not Say
-              </label>
-            </div>
+          <div v-if="mode === 'edit'" class="w-full max-w-md">
+            <RadioGroup v-model="editGender">
+              <RadioGroupLabel class="sr-only">Server size</RadioGroupLabel>
+              <div class="flex flex-wrap justify-between">
+                <RadioGroupOption
+                  as="template"
+                  v-for="g in genders"
+                  :key="g"
+                  :value="g"
+                  v-slot="{ active, checked }"
+                  @click="editGender === g"
+                >
+                  <div
+                    :class="[
+                      active
+                        ? 'ring-1 ring-offset-1 ring-offset-[#B163E2] ring-white ring-opacity-60'
+                        : '',
+                      checked
+                        ? 'bg-light bg-opacity-80 text-white '
+                        : 'bg-white ',
+                    ]"
+                    class="flex py-2 px-5 mt-3 w-auto rounded-lg shadow-md cursor-pointer focus:outline-none transition ease-in-out"
+                  >
+                    <div class="flex items-center justify-between">
+                      <div class="flex items-center">
+                        <div
+                          v-show="checked"
+                          class="flex-shrink-0 text-white mr-2"
+                        >
+                          <svg class="w-3 h-3" viewBox="0 0 24 24" fill="none">
+                            <circle
+                              cx="12"
+                              cy="12"
+                              r="12"
+                              fill="#fff"
+                              fill-opacity="0.2"
+                            />
+                            <path
+                              d="M7 13l3 3 7-7"
+                              stroke="#fff"
+                              stroke-width="1.5"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            />
+                          </svg>
+                        </div>
+                        <div class="text-xs">
+                          <RadioGroupLabel
+                            as="p"
+                            :class="checked ? 'text-white' : 'text-gray-900'"
+                            class="font-normal text-center font-aileron"
+                          >
+                            {{ g }}
+                          </RadioGroupLabel>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </RadioGroupOption>
+              </div>
+            </RadioGroup>
           </div>
           <p
             v-if="mode === 'view'"
@@ -143,7 +148,7 @@
             v-if="mode === 'edit'"
             v-model="editAbout"
             type="text"
-            @input="editAbout = event.target.value"
+            @input="(event) => (editAbout = event.target.value)"
             class="bg-[#ECECEC] text-xs text-black border border-grey w-full px-2 py-2 rounded-xl font-poppins"
           />
           <p
@@ -159,7 +164,7 @@
             v-if="mode === 'edit'"
             v-model="editLocation"
             type="text"
-            @input="editLocation = event.target.value"
+            @input="(event) => (editLocation = event.target.value)"
             class="bg-[#ECECEC] text-xs text-black border border-grey w-full px-2 py-2 rounded-xl font-poppins"
           />
           <p
@@ -169,19 +174,39 @@
             {{ location ?? "NA" }}
           </p>
         </div>
+        <button
+          disabled
+          v-if="loading && mode === 'edit'"
+          @click="submit"
+          class="font-aileron flex flex-initial w-full bg-primary uppercase text-white text-center justify-center font-bold rounded-full py-2 px-2 lg:px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition duration-300 ease-in-out text-xs disabled:cursor-not-allowed"
+        >
+          <img class="h-6 w-6" src="../../public/assets/icon/loader.svg" />
+        </button>
+        <button
+          v-if="!loading && mode === 'edit'"
+          @click="editProfile()"
+          class="flex font-aileron flex-initial w-full bg-primary uppercase text-white text-center justify-center font-bold rounded-full py-3 px-4 lg:px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out text-xs"
+        >
+          Submit
+        </button>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
+import { baseURL } from "@/constants";
 import {
   RadioGroup,
   RadioGroupDescription,
   RadioGroupLabel,
   RadioGroupOption,
 } from "@headlessui/vue";
-import { defineComponent } from "vue";
+import axios from "axios";
+import { defineComponent, ref } from "vue";
+import { useToast } from "vue-toastification";
+import { mapActions, mapState } from "vuex";
 import Input from "./UI/Input.vue";
+const genders = ["Male", "Female", "Other"];
 
 export default defineComponent({
   components: {
@@ -195,12 +220,14 @@ export default defineComponent({
   data() {
     return {
       mode: "view",
+      loading: false,
       editName: this.name,
       editUserName: this.userName,
-      editLocation: this.location,
+      editLocation: this.location ?? "",
       editEmail: this.email,
-      editGender: this.gender,
-      editAbout: this.about,
+      editGender: this.gender ?? "",
+      editAbout: this.about ?? "",
+      selectedView: "normal",
     };
   },
   props: {
@@ -212,5 +239,58 @@ export default defineComponent({
     about: String,
   },
   mounted() {},
+  setup() {
+    const toast = useToast();
+
+    return {
+      genders,
+      toast,
+    };
+  },
+  computed: mapState({
+    token: (state: any) => state.user.token,
+  }),
+  methods: {
+    editProfile() {
+      if (
+        this.editName?.trim().length === 0 ||
+        this.editUserName?.trim().length === 0 ||
+        this.editLocation?.trim().length === 0 ||
+        this.editAbout?.trim().length === 0 ||
+        this.editGender?.trim().length === 0
+      ) {
+        this.toast.error("Please fill in all fields");
+      } else {
+        this.loading = true;
+        const payload = {
+          userName: this.editUserName,
+          gender: this.editGender,
+          about: this.editAbout,
+          location: this.editLocation,
+          name: this.editName,
+        };
+        axios
+          .put(`${baseURL}/user/profile`, payload, {
+            headers: { Authorization: `Bearer ${this.token}` },
+          })
+          .then((res) => {
+            this.loading = false;
+            this.setUserDetails({ ...payload, email: this.email });
+            this.toast.success("Profile Edited Successfully!");
+            this.mode = "view";
+          })
+          .catch((error) => {
+            this.loading = false;
+            this.toast.error(
+              error.response.data.error ??
+                "Something went wrong. Please try again"
+            );
+          });
+      }
+    },
+    ...mapActions("user", {
+      setUserDetails: "setUserDetails",
+    }),
+  },
 });
 </script>
