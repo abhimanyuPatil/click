fd3df9dc5823cce866ff6382533ebc57030be04b
Abhimanyu Patil <chanondler@Abhimanyus-MacBook-Air.local>
2022-06-11 00:48:05 +0530

    description modal

--- src/components/Cards/GridLayoutCard.vue
+++ src/components/Cards/GridLayoutCard.vue
@@ -149,14 +149,30 @@
             leave-to="opacity-0 scale-95"
           >
             <DialogPanel
-              class="w-full max-w-[90%] h-[90vh] transform overflow-hidden rounded-2xl bg-white align-middle shadow-xl transition-all"
+              class="w-full max-w-[90%] h-[90vh] transform overflow-hidden rounded-none bg-white align-middle shadow-xl transition-all"
             >
               <div class="flex">
-                <div class="flex w-[70%] rounded-none">
+                <div class="relative flex w-[70%] rounded-none">
                   <img :src="currentItem?.image" class="w-full object-cover" />
+                  <div class="absolute flex items-center top-36 right-5">
+                    <div
+                      class="rounded-full px-4 py-2 text-xs text-white mx-3"
+                      v-for="opt in ['Save', 'Permalink', 'Book']"
+                      :key="opt"
+                      :class="
+                        opt === 'Book'
+                          ? 'bg-primary'
+                          : 'bg-[#000000] bg-opacity-50 '
+                      "
+                    >
+                      {{ opt }}
+                    </div>
+                  </div>
                 </div>
-                <div class="max-w-[30%] flex flex-col">
-                  <div class="bg-white p-4 text-left">
+                <div class="max-w-[30%] flex flex-col justify-between">
+                  <div
+                    class="px-4 py-1 text-left flex flex-col basis-[55%] justify-end"
+                  >
                     <div class="portfolio-title flex items-start px-3">
                       <div
                         class="p-[5px] md:p-[10px] rounded-sm mr-2"
@@ -211,8 +227,111 @@
                     <p class="text-xs px-3 text-grey">
                       {{ currentItem?.description }}
                     </p>
+                    <div class="flex items-center mt-10">
+                      <img
+                        class="h-10 w-10 object-scale-down flex basis-[10%]"
+                        src="../../../resources/dp.png"
+                      />
+                      <div
+                        class="text-[#686868] font-poppins text-xs flex flex-wrap basis-[50%]"
+                      >
+                        Sk and
+                        <p class="text-orange">&nbsp;136&nbsp;</p>
+                        Liked,
+                        <p class="text-orange">&nbsp;320&nbsp;</p>
+                        viewed this post.
+                      </div>
+                      <div class="flex items-center justify-around basis-[40%]">
+                        <div
+                          class="justify-center flex items-center h-6 bg-[#ECEFF4] dark:bg-dropdownBg shadow-md rounded-2xl mb-2 p-3 group transition sm:w-fit cursor-pointer hover:bg-[#6317A7] dark:hover:bg-[#6317A7] lg:m-0 lg:h-7"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            width="16"
+                            height="16"
+                            class="cursor-pointer fill-grey dark:fill-darkWhite group-hover:fill-white"
+                          >
+                            <path
+                              d="M5.5 15a3.51 3.51 0 0 0 2.36-.93l6.26 3.58a3.06 3.06 0 0 0-.12.85 3.53 3.53 0 1 0 1.14-2.57l-6.26-3.58a2.74 2.74 0 0 0 .12-.76l6.15-3.52A3.49 3.49 0 1 0 14 5.5a3.35 3.35 0 0 0 .12.85L8.43 9.6A3.5 3.5 0 1 0 5.5 15zm12 2a1.5 1.5 0 1 1-1.5 1.5 1.5 1.5 0 0 1 1.5-1.5zm0-13A1.5 1.5 0 1 1 16 5.5 1.5 1.5 0 0 1 17.5 4zm-12 6A1.5 1.5 0 1 1 4 11.5 1.5 1.5 0 0 1 5.5 10z"
+                            ></path>
+                          </svg>
+                        </div>
+                        <div
+                          class="flex justify-center items-center h-6 bg-[#ECEFF4] dark:bg-dropdownBg shadow-md rounded-2xl mb-2 p-3 group transition sm:w-fit cursor-pointer hover:bg-[#6317A7] dark:hover:bg-[#6317A7] lg:m-0 lg:h-7"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            viewBox="0 0 24 24"
+                            class="cursor-pointer fill-grey dark:fill-darkWhite group-hover:fill-white"
+                          >
+                            <path
+                              d="M20.205 4.791a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412L12 21.414l8.207-8.207c2.354-2.353 2.355-6.049-.002-8.416z"
+                            ></path>
+                          </svg>
+                        </div>
+                        <div
+                          class="flex justify-center cursor-pointer items-center h-6 mb-2 bg-[#ECEFF4] dark:bg-dropdownBg shadow-md p-3 rounded-2xl group transition sm:w-fit hover:bg-[#6317A7] dark:hover:bg-[#6317A7] lg:m-0 lg:h-7"
+                        >
+                          <img
+                            class="h-4 w-4 object-fit"
+                            src="../../../public/assets/icon/edit-icon.svg"
+                            alt="edit-icon"
+                          />
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div class="p-4 bg-gray-200 flex justify-between">
+                  <div
+                    class="flex flex-col basis-[35%] border-t-[1px] p-3 border-[#707070] overflow-y-scroll bg-white"
+                  >
+                    <div
+                      class="border-[1px] border-[#B96AFF] bg-[#FAF5FF] text-[#6317A7] text-xs py-3 text-center rounded-lg"
+                    >
+                      Verified user only can comment on this post
+                    </div>
+                    <div :key="i" v-for="i in 3" class="flex items-start my-4">
+                      <img
+                        class="h-10 w-10 object-scale-down"
+                        src="../../../resources/dp.png"
+                      />
+                      <div class="text-left pl-3">
+                        <h4 class="font-aileron text-[#343434]">
+                          Alex Philips
+                        </h4>
+                        <p class="text-xs text-[#9B9B9B]">@alex</p>
+                        <p class="text-[#6A6868] text-sm">Amazing work</p>
+                        <div class="flex items-center">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            viewBox="0 0 24 24"
+                            class="cursor-pointer fill-[#5E5C5C] dark:fill-darkWhite group-hover:fill-white"
+                          >
+                            <path
+                              d="M20.205 4.791a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412L12 21.414l8.207-8.207c2.354-2.353 2.355-6.049-.002-8.416z"
+                            ></path>
+                          </svg>
+                          <p
+                            class="text-xs ml-2 text-[#5E5C5C] dark:text-darkWhite font-aileron group-hover:text-white"
+                          >
+                            2 likes
+                          </p>
+                          <p
+                            class="text-xs ml-4 text-[#5E5C5C] dark:text-darkWhite font-aileron group-hover:text-white"
+                          >
+                            4 days ago
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    class="p-2 bg-[#F3F3F3] flex justify-between basis-[8%] items-center"
+                  >
                     <div class="flex items-center">
                       <img
                         class="h-10 w-10 object-scale-down"
